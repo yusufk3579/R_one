79551a36aef6b79e911c8726659b84645d557705
yusufk3579 <yusufkhan3579@gmail.com>
2019-03-02 18:05:38 +0530

    Create one.R

---
+++ one.R
@@ -0,0 +1,32 @@
+c(0,1,2,3,4,5,6)
+c
+1:50
+cos(3.14)
+sin(3.14)
+d<-1+34
+d
+add()
+sum
+f <- function(x,y) {c(x+1, y+1)}
+f(1,2)
+
+
+a <- array(c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12),dim=c(3,4))
+a
+v <- c(1,2,3,4,5,6,7,8,9,10,11,12)
+m<-matrix(c(1,2,3,4,5,6,7,8,9,10,11,12),nrow=3,ncol=4)
+w <- array(c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18),dim=c(3,3,3))
+w
+
+#a list containing two strings
+e<- list(thing="hat", size=8.25)
+e
+
+View(e)
+teams<-c("phi", "nym", "fla", "atl", "wsn")
+w <-c(92, 89, 94, 72, 59)
+l<-c(70, 73, 77, 90, 102)
+nleast <- data.frame(teams,w,l)
+nleast
+
+
